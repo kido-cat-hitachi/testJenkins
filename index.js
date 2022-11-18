2862e3116ea19a83a0443e5e9080bd53a03685f3
kido-cat <mathenku1998@gmail.com>
2022-11-18 16:35:32 +0700

    add the html report

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export default function() {
-    let res = http.get('https://google.com');
+    http.get('https://test.k6.io');
 };
+
+
+export function handleSummary(data) {
+    return {
+        "report.html" : htmlReport(data),
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
+}
